5dd894d6dfe8e91f4ff31966e04c7833d8ad6b26
javier <jgalvarez.jga@gmail.com>
2020-08-26 08:26:29 -0700

    cambiar array simpleXML, añadir Soap

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -27,6 +27,59 @@ class DefaultController extends Controller
     }
 
     /**
+     * @Route("/cargar", name="cargar_fichero")
+     */
+    public function cargar_ficheroAction(Request $request) //, CargarXml $cargarxml)
+    {                            
+        //lanzar el servicio
+        //$cargarxml = $this->get('cargarxml');        
+        //$objects = $cargarxml->loadFile();
+                
+
+        //$form = $this->createFormBuilder($objects)            
+        $form = $this->createFormBuilder()            
+            ->add('ficheroxml', FileType::class, [ 'label' => 'Fichero XML'])            
+            ->add('subir', SubmitType::class, array(
+                'label' => 'Ejecutar',                                             
+               
+             /* 
+                'constraints' => [
+                    new File([
+                        'maxSize' => '1024k',
+                        'mimeTypes' => [
+                            'application/xml',                            
+                        ]])
+                          ],
+               */
+
+                ))                       
+            ->getForm();            
+
+             //formulario recibido de la vista        
+            $form->handleRequest($request);                                    
+
+            if ($form->isValid()) {
+            //if ($form->isSubmitted() && $form->isValid()) {
+                // /** @var UploadedFile $file */
+                $file = $form->get('ficheroxml')->getData();                                                
+                $originalFilename = pathinfo($file->getClientOriginalName(), PATHINFO_FILENAME);                           
+
+                if ($file) {
+                    //$cargarxml = $this->get('cargarxml');  
+                    //$fileName = $cargarxml->loadFile($file);                                      
+                    //dump($filename);
+                    return $this->redirectToRoute('homepage'); 
+                }              
+            }                
+
+        return $this->render('default/form_file.html.twig', [
+            //'dato' => $datos,
+            'formulario' => $form->createView(),
+        ]);                        
+    }
+
+
+    /**
      * @Route("/leer_fichero", name="leer_fichero")
      */
     public function leerficheroAction(Request $request)
@@ -34,7 +87,6 @@ class DefaultController extends Controller
                      
         $nameFile = 'file:///home/javier/Desktop/cdv/parse_xml/app/Resources/Ficheros/RS_availability_no_soap.xml';        
         
-
         //$nameFile = file_get_contents('/../Resources/Ficheros/RS_availability.xml');                
         if (file_exists($nameFile)) {            
             $xml = simplexml_load_file($nameFile);                        
@@ -84,12 +136,11 @@ class DefaultController extends Controller
 
             $tiempo_final = microtime(true);
             $tiempo = $tiempo_final - $tiempo_inicial;  
-            echo $tiempo. " segundos";
+            //echo $tiempo. " segundos";
 
         } else {
             exit('No se ha cargo el fichero o no existe');
         }
-
 
         //recuperamos el servicio serializer
         //$serializer = $this->get('serializer');
@@ -102,60 +153,94 @@ class DefaultController extends Controller
     }
 
     /**
-     * @Route("/cargar", name="cargar_fichero")
+     * @Route("/leer_fichero_soap", name="leer_fichero_soap")
      */
-    public function cargar_ficheroAction(Request $request) //, CargarXml $cargarxml)
-    {                            
-        //lanzar el servicio
-        //$cargarxml = $this->get('cargarxml');        
-        //$objects = $cargarxml->loadFile();
-                
+    public function leerficheroSoapAction(Request $request)
+    {        
+        $nameFile = 'file:///home/javier/Desktop/cdv/parse_xml/app/Resources/Ficheros/RS_availability.xml';                   
+        if (file_exists($nameFile)) {       
 
-        //$form = $this->createFormBuilder($objects)            
-        $form = $this->createFormBuilder()            
-            ->add('ficheroxml', FileType::class, [ 'label' => 'Fichero XML'])            
-            ->add('subir', SubmitType::class, array(
-                'label' => 'Ejecutar',                                             
-               
-             /* 
-                'constraints' => [
-                    new File([
-                        'maxSize' => '1024k',
-                        'mimeTypes' => [
-                            'application/xml',                            
-                        ]])
-                          ],
-               */
-
-                ))                       
-            ->getForm();            
-
-             //formulario recibido de la vista        
-            $form->handleRequest($request);                                    
-
-            if ($form->isValid()) {
-            //if ($form->isSubmitted() && $form->isValid()) {
-                // /** @var UploadedFile $file */
-                $file = $form->get('ficheroxml')->getData();                                                
-                $originalFilename = pathinfo($file->getClientOriginalName(), PATHINFO_FILENAME);                           
-
-                if ($file) {
-                    //$cargarxml = $this->get('cargarxml');  
-                    //$fileName = $cargarxml->loadFile($file);                                      
-                    //dump($filename);
-                    return $this->redirectToRoute('homepage'); 
-                }              
-            }
-    
-            
-
-        return $this->render('default/form_file.html.twig', [
-            //'dato' => $datos,
-            'formulario' => $form->createView(),
-        ]);                
+            $xml = simplexml_load_file($nameFile)                         
+                ->children('http://schemas.xmlsoap.org/soap/envelope/')
+                ->Body->children('http://www.opentravel.org/OTA/2003/05'); 
         
+            //var_dump($xml);
+            
+            $tiempo_inicial = microtime(true);            
+        
+            //$array_simple_xml = []; 
+                
+            $z = 0;
+            for ($i = 0; $i < count($xml->OTA_HotelAvailServiceResponse->OTA_HotelAvailRS->RoomStays->RoomStay); $i++) {   
+                $j = 0;                        
+                    foreach ($xml->OTA_HotelAvailServiceResponse->OTA_HotelAvailRS->RoomStays->RoomStay[$i]->BasicPropertyInfo  as $hotel)                 
+                        {                                                                       
+                        $HotelCode = $hotel->attributes()->HotelCode;                                    
+                        //print_r( $HotelCode ."</br>");                        
+                    }                  
 
+                    $z = count($xml->OTA_HotelAvailServiceResponse->OTA_HotelAvailRS->RoomStays->RoomStay[$i]->RoomRates[$j]->RoomRate);                            
+                        
+                    while ($j < $z) 
+                    {       
+                        foreach ($xml->OTA_HotelAvailServiceResponse->OTA_HotelAvailRS->RoomStays->RoomStay[$i]->RoomRates->RoomRate[$j]->Rates->Rate as $elemento1)                 
+                            {                                   
+                                $NombreHabitacion = $elemento1->RateDescription->Text; 
+                                $Precio = $elemento1->Total->attributes()->AmountAfterTax;                                                               
+                                //print_r( $NombreHabitacion ."</br>");
+                                //print_r( $Precio ."</br>");                                                                                                                                               
+                            
+                            }
+                            $j++;   
+                            
+                            $array_simple_xml[$j] = array("Código Alojamiento ".$i, array( "Código"=> $HotelCode, "room_list" => $NombreHabitacion, "Total " => $Precio));        
+                                                        
+                    }        
+                        
+                    //Agrupar array por codigo de alojamiento
+
+                    //
+
+                    /*
+                    foreach ($xml->OTA_HotelAvailServiceResponse->OTA_HotelAvailRS->RoomStays->RoomStay[$i]->RoomRates->RoomRate[$j]->TPA_Extensions as $elemento2)                      
+                        {                                                                                
+                            $RegimenTarifa = $elemento2->Mealplan->attributes()->Category;                         
+                            $array_simple_xml[] = $RegimenTarifa;             
+                            //$array_simple_xml["regimenTarifa"] = $RegimenTarifa;                            
+                            print_r( $RegimenTarifa ."</br>");
+                        }                                   
+                     */                                                                             
+            }        
+        
+                //var_dump($array_simple_xml);                      
+                $tiempo_final = microtime(true);
+                $tiempo = $tiempo_final - $tiempo_inicial;                    
+        
+        } else {
+            exit('no se ha cargado el archivo');
+        }
+
+        
+        return $this->render('default/read_file.html.twig', array(            
+            'data' => $array_simple_xml,
+            'eficiencia' => $tiempo,                     
+         ));                
+        
     }
 
+    /**
+     * @Route("/leer_fichero_dom", name="leer_fichero_dom")
+     */
+    public function leerficheroDomAction(Request $request)
+    {                            
+        $data = 1;
+        $eficiencia = 1;
+
+        return $this->render('default/read_file.html.twig', array(            
+            'data' => $array_simple_xml,
+            'eficiencia' => $tiempo,                     
+         ));           
+    }
+    
 
 }
